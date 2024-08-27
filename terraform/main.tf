e9dcad72799d33a692900750c71a126fae545a38
Sergio Gordon <gordon.sergio9@gmail.com>
2024-08-26 23:14:20 -0500

    terraform updates

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,7 +18,72 @@ resource "aws_elastic_beanstalk_environment" "voxinsights_env" {
     value     = aws_iam_instance_profile.eb_instance_profile.name
   }
 
-  # Add more settings as needed
+  setting {
+    namespace = "aws:ec2:vpc"
+    name      = "VPCId"
+    value     = var.vpc_id
+  }
+
+  setting {
+    namespace = "aws:ec2:vpc"
+    name      = "Subnets"
+    value     = join(",", var.subnet_ids)
+  }
+
+  setting {
+    namespace = "aws:elasticbeanstalk:environment"
+    name      = "EnvironmentType"
+    value     = "LoadBalanced"
+  }
+
+  setting {
+    namespace = "aws:elasticbeanstalk:environment"
+    name      = "LoadBalancerType"
+    value     = "application"
+  }
+
+  setting {
+    namespace = "aws:autoscaling:launchconfiguration"
+    name      = "SecurityGroups"
+    value     = aws_security_group.eb_sg.id
+  }
+
+  setting {
+    namespace = "aws:elasticbeanstalk:application:environment"
+    name      = "ENVIRONMENT"
+    value     = var.environment_name
+  }
+
+  tags = {
+    Environment = var.environment_name
+    Project     = "VoxInsights"
+  }
+}
+
+resource "aws_security_group" "eb_sg" {
+  name        = "eb-security-group"
+  description = "Security group for Elastic Beanstalk environment"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name        = "EB Security Group"
+    Environment = var.environment_name
+    Project     = "VoxInsights"
+  }
 }
 
 # IAM role and instance profile
@@ -47,6 +112,11 @@ resource "aws_iam_instance_profile" "eb_instance_profile" {
 # Attach necessary policies to the role
 resource "aws_iam_role_policy_attachment" "eb_web_tier" {
   policy_arn = "arn:aws:iam::aws:policy/AWSElasticBeanstalkWebTier"
+  role       = aws_iam_role.eb_instance_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "eb_ssm_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
   role       = aws_iam_role.eb_instance_role.name
 }
 
